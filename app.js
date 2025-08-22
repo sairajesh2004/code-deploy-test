af7d49498a0d4b79e0ea80cd995856a4e5725045
Chenna Sai Rajesh <138917097+sairajesh2004@users.noreply.github.com>
2025-08-22 13:28:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.use(express.json())
 const path = require('path')
 const dbpath = path.join(__dirname, 'userData.db')
 
-const {open} = require('sqlite')
+const { open } = require('sqlite')
 const sqlite3 = require('sqlite3')
 
 const bcrypt = require('bcrypt')
@@ -18,12 +18,10 @@ const intializeserver = async () => {
       filename: dbpath,
       driver: sqlite3.Database,
     })
-    const PORT = process.env.PORT || 3000
 
-    intializeserver().then(() => {
-      app.listen(PORT, () => {
-        console.log('Server running at http://localhost:3000')
-      })
+    const PORT = process.env.PORT || 3000
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}`)
     })
   } catch (e) {
     console.log(`DB message: ${e.message}`)
@@ -31,67 +29,64 @@ const intializeserver = async () => {
   }
 }
 
+intializeserver()
+
+// ---------------- API ROUTES ---------------- //
+
 app.post(`/register`, async (request, response) => {
-  const {username, name, password, gender, location} = request.body
-  if (request.body.password.length < 5) {
-    response.status(400)
-    response.send('Password is too short')
+  const { username, name, password, gender, location } = request.body
+  if (password.length < 5) {
+    response.status(400).send('Password is too short')
   } else {
-    const hashedpwd = await bcrypt.hash(request.body.password, 10)
-    const thequery = `select * from user where username = '${username}';`
+    const hashedpwd = await bcrypt.hash(password, 10)
+    const thequery = `SELECT * FROM user WHERE username = '${username}';`
     const theanswer = await dbase.get(thequery)
     if (theanswer === undefined) {
-      const createquery = `insert into user(username,name,password,gender,location) values('${username}','${name}','${hashedpwd}','${gender}','${location}');`
-      const afterregis = await dbase.run(createquery)
-      const regisid = afterregis.lastId
-      response.status(200)
-      response.send('User created successfully')
+      const createquery = `
+        INSERT INTO user(username,name,password,gender,location) 
+        VALUES('${username}','${name}','${hashedpwd}','${gender}','${location}');
+      `
+      await dbase.run(createquery)
+      response.status(200).send('User created successfully')
     } else {
-      response.status(400)
-      response.send('User already exists')
+      response.status(400).send('User already exists')
     }
   }
 })
 
 app.post(`/login`, async (request, response) => {
-  const {username, password} = request.body
-  const thequery = `select * from user where username = '${username}';`
+  const { username, password } = request.body
+  const thequery = `SELECT * FROM user WHERE username = '${username}';`
   const theanswer = await dbase.get(thequery)
   if (theanswer === undefined) {
-    response.status(400)
-    response.send('Invalid user')
+    response.status(400).send('Invalid user')
   } else {
     const yesorno = await bcrypt.compare(password, theanswer.password)
     if (yesorno) {
-      response.status(200)
-      response.send('Login success!')
+      response.status(200).send('Login success!')
     } else {
-      response.status(400)
-      response.send('Invalid password')
+      response.status(400).send('Invalid password')
     }
   }
 })
 
 app.put(`/change-password`, async (request, response) => {
-  const {username, oldPassword, newPassword} = request.body
-  const thequery = `select * from user where username = '${username}';`
+  const { username, oldPassword, newPassword } = request.body
+  const thequery = `SELECT * FROM user WHERE username = '${username}';`
   const theanswer = await dbase.get(thequery)
   if (theanswer !== undefined) {
     const yesorno = await bcrypt.compare(oldPassword, theanswer.password)
     if (yesorno) {
-      if (request.body.newPassword.length < 5) {
-        response.status(400)
-        response.send('Password is too short')
+      if (newPassword.length < 5) {
+        response.status(400).send('Password is too short')
       } else {
         const newhashedpwd = await bcrypt.hash(newPassword, 10)
-        const updtquery = `update user set password = '${newhashedpwd}' where username= '${username}';`
+        const updtquery = `UPDATE user SET password = '${newhashedpwd}' WHERE username= '${username}';`
         await dbase.run(updtquery)
-        response.status(200)
-        response.send('Password updated')
+        response.status(200).send('Password updated')
       }
     } else {
-      response.status(400)
-      response.send('Invalid current password')
+      response.status(400).send('Invalid current password')
     }
   }
 })
